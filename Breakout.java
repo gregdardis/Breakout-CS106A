7802c5fa283a87db92ea652777eff4422fa177fd
Greg Dardis <dardis.greg@gmail.com>
2017-05-02 16:39:20 -0600

    added moving paddle, bouncing ball, bricks dissappear, win/lose the game, changable number of rounds, etc

--- Breakout.java
+++ Breakout.java
@@ -49,13 +49,24 @@ public class Breakout extends GraphicsProgram {
 		private static final int BRICK_HEIGHT = 8;
 
 	/** Radius of the ball in pixels */
-		private static final int BALL_RADIUS = 10;
+		private static final int BALL_DIAMETER = 15;
+		
+	/** Speed of the ball */
+		private static final int BALL_SPEED = 1;
+		
+	/** Pause time for the ball */
+		private static final int BALL_PAUSE_TIME = 4;
 
 	/** Offset of the top brick row from the top */
 		private static final int BRICK_Y_OFFSET = 70;
 
 	/** Number of turns */
 		private static final int N_OF_TURNS = 3;
+		
+	/** How long to pause after paddle movement */
+		private static final int PADDLE_PAUSE_TIME = 10;
+		
+	
 		
 		/* Finds the x value of the top left corner of the first brick so the bricks
 		 * can be drawn centered in the application window in method "createRowsOfBricks()"
@@ -124,14 +135,14 @@ public class Breakout extends GraphicsProgram {
 		}
 		
 		/* Gets the x coordinate for the starting position of the paddle */
-		private int getXForPaddle() {
+		private int getStartingXForPaddle() {
 			int x = 0;
 			x = (APPLICATION_WIDTH - PADDLE_WIDTH) / 2;
 			return x;
 		}
 		
 		/* Gets the y coordinate for the starting position of the paddle */
-		private int getYForPaddle() {
+		private int getStartingYForPaddle() {
 			int y = 0;
 			y = APPLICATION_HEIGHT - PADDLE_Y_OFFSET - PADDLE_HEIGHT;
 			return y;
@@ -139,8 +150,8 @@ public class Breakout extends GraphicsProgram {
 		
 		/* Draws the starting paddle and places it in the middle of the screen at the correct height */
 		private GObject createPaddle() {
-			int x = getXForPaddle();
-			int y = getYForPaddle();
+			int x = getStartingXForPaddle();
+			int y = getStartingYForPaddle();
 			GRect paddle = new GRect(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
 			paddle.setColor(Color.BLACK);
 			paddle.setFilled(true);
@@ -149,55 +160,261 @@ public class Breakout extends GraphicsProgram {
 			return paddle;
 		}
 		
+		/* Returns the x value for the top left corner of the ball, 
+		 * which is in the middle of the screen */
+		private int getStartingXForBall() {
+			int x = 0;
+			x = (APPLICATION_WIDTH - BALL_DIAMETER) / 2;
+			return x;
+		}
+		
+		/* Returns the y value for the top left corner of the ball, 
+		 * which is in the middle of the screen */
+		private int getStartingYForBall() {
+			int y = 0;
+			y = (APPLICATION_HEIGHT / 2);
+			return y;
+		}
+		
+		/* Creates a new ball object that is black, and returns it */
+		private GObject createBall() {
+			int x = getStartingXForBall();
+			int y = getStartingYForBall();
+			GOval ball = new GOval(x, y, BALL_DIAMETER, BALL_DIAMETER);
+			ball.setColor(Color.BLACK);
+			ball.setFilled(true);
+			ball.setFillColor(Color.BLACK);
+			add(ball);
+			return ball;
+		}
+		
 		/** Called on mouse drag to reposition the object */
 		public void mouseMoved(MouseEvent e) {
-			if (paddle != null) {
-//				y might be PADDLE_HEIGHT?
-				paddle.move(e.getX() - lastX, getYForPaddle() + (PADDLE_HEIGHT / 2));
-				lastX = e.getX();
-//				lastY = e.getY();
-//				y used to be same as x but for y
+			
+				// if paddle is on screen then move
+				if (paddle.getX() >= 0 && paddle.getX() <= (getWidth() - PADDLE_WIDTH)) {
+					paddle.move(e.getX() - (paddle.getX() + (PADDLE_WIDTH / 2)), 0);
+				}
+				
+				// if paddle moves off left side of screen then put it on the far left, but on the screen
+				if (paddle.getX() < 0) {
+					paddle.setLocation(0, getStartingYForPaddle());
+				}
+				
+				// if paddle moves off right side of screen the put it on the far right, but on the screen
+				if (paddle.getX() > (getWidth() - PADDLE_WIDTH)) {
+					paddle.setLocation(getWidth() - PADDLE_WIDTH, getStartingYForPaddle());
+				}
+			pause(PADDLE_PAUSE_TIME);
+		}
+		
+		/* When a new ball spawns, call this method and it will change
+		 * the vx (x velocity) to a random number between 1 and BALL_SPEED, or -1 and -BALL_SPEED */
+		private void chooseBallXDirectionAndSpeed() {
+			vx = rgen.nextDouble(1.0, BALL_SPEED);
+			if (rgen.nextBoolean(0.5)) vx = -vx;
+		}
+		
+		/* Gives both the x starting direction and speed and combines that with the
+		 * starting vy speed (velocity of y downward, it starts positive) into one method */
+		private void chooseStartingBallDirectionAndSpeed() {
+			chooseBallXDirectionAndSpeed();
+			vy = BALL_SPEED;
+		}
+		
+		/* Moves the ball with velocity vx, vy, then pauses a short time */
+		private void moveBall() {
+			ball.move(vx, vy);
+			pause(BALL_PAUSE_TIME);
+		}
+		
+		/* checks if the ball hits a wall, if it does then angle of incidence = angle of reflection */
+		private void checkWallCollision() {
+			/* If the ball hits the right wall, reverse x velocity */
+			if (ball.getX() >= (getWidth() - BALL_DIAMETER)) {
+				vx = -vx;
+			}
+			/* If the ball hits the left wall, reverse x velocity */
+			if (ball.getX() <= 0) {
+				vx = -vx;
+			}
+			/* If the ball hits the ceiling, reverse y velocity */
+			if (ball.getY() <= 0) {
+				vy = -vy;
 			}
 		}
 		
-		private void setUpGame() {
-			createRowsOfBricks();
-			createPaddle();
-			initializeMouse();
+		/* Checks to see if the ball hits the bottom wall.
+		 * If it does, increment failcounter and remove the ball from the screen.
+		 * If they have any turns left, create a new ball but set the round started to false
+		 * If they have no turns left, end the game */
+		private void checkBottomWallCollision() {
+			if (ball.getY() >= (getHeight() - BALL_DIAMETER)) {
+				failCounter++;
+				remove(ball);
+				
+				if (failCounter < N_OF_TURNS) {
+					ball = createBall();
+					roundStarted = false;
+				}
+				else {
+					gameOverScreen();
+				}
+			}
 		}
 		
-//		private void playGame() {
+		/* Method that's called when you run out of lives */
+		private void gameOverScreen() {
+			GLabel gameOver = new GLabel("Game over");
+			gameOver.setFont("Arial-46");
+			gameOver.setLocation(((getWidth() - gameOver.getWidth()) / 2), ((getHeight() - gameOver.getHeight()) / 2));  
+			gameOver.setColor(Color.RED);
+			add(gameOver);
+			pause(5000);
+			System.exit(0);
+		}
 		
-//		}
+		/* Checks if the ball hits a brick, if it does then removes the brick
+		 * and the angle of incidence = angle of reflection */
+		private void checkBrickCollision() {
+			/* finding all of the points around the ball on 4 sides, going clockwise starting at top left */
+			double x1 = ball.getX();
+			double y1 = ball.getY();
+			
+			double x2 = ball.getX() + (BALL_DIAMETER);
+			double y2 = ball.getY();
+			
+			double x3 = ball.getX() + (BALL_DIAMETER);
+			double y3 = ball.getY() + (BALL_DIAMETER);	
+			
+			double x4 = ball.getX();
+			double y4 = ball.getY() + (BALL_DIAMETER);
+			
+			/* Checks each corner of the ball in turn to see if it collided with an object*/
+			GObject collidingObject = getCollidingObject(x1, y1);
+			if (collidingObject == null) {
+				collidingObject = getCollidingObject(x2, y2);
+				if (collidingObject == null) {
+					collidingObject = getCollidingObject(x3, y3);
+					if (collidingObject == null) {
+						collidingObject = getCollidingObject(x4, y4);
+					}
+				}
+			}
+			
+			/* If the ball collides with an object, either just reverse y velocity if it collided with the paddle
+			 * or reverse the y velocity and remove the object it collided with if it collided with a brick.
+			 * Walls do not count as objects */
+			if (collidingObject != null) {
+				if (collidingObject == paddle) {
+					vy = -vy;
+				}
+				else {
+					vy = -vy;
+					remove(collidingObject);
+					numberOfBricksRemaining--;
+					if (numberOfBricksRemaining == 0) {
+						youWinScreen();
+					}
+				}
+			}
+			
+		}
+		/* Method that's called when you clear all the bricks */
+		private void youWinScreen() {
+			GLabel youWin = new GLabel("You win!");
+			youWin.setFont("Arial-46");
+			youWin.setLocation(((getWidth() - youWin.getWidth()) / 2), ((getHeight() - youWin.getHeight()) / 2));  
+			youWin.setColor(Color.BLUE);
+			add(youWin);
+			pause(5000);
+			System.exit(0);
+		}
 		
-		/* Prepares the mouse to control the paddle, paddle is set to middle of the paddle's location */
+		
+		/* checks a point with (x, y) coordinates and if there is an object there, returns that object.
+		 * otherwise, returns null */ 
+		private GObject getCollidingObject(double x, double y) {
+			if (getElementAt(x, y) != null) {
+				return getElementAt(x, y);
+			}
+			else return null;
+		}
+		
+		/* Sets up the game */
+		private void setUpGame() {
+			createRowsOfBricks();
+			initializeMouse();
+			ball = createBall();
+		}
+		
+		/* Plays the game */
+		private void playGame() {
+			while(true) {
+				boolean roundReset = false;
+//				System.out.println(failCounter);
+//				System.out.println("looping this shit?");
+				if (!roundReset && !roundStarted) {
+					roundReset = true;
+					chooseStartingBallDirectionAndSpeed();
+//					GLabel clickAnywhere = addClickAnywhere();
+					add(clickAnywhere);
+//					System.out.println("Got to here");
+				}
+				while(roundStarted) {
+//					System.out.println("Round started!");
+					remove(clickAnywhere);
+					moveBall();
+					checkBrickCollision();
+					checkWallCollision();
+					checkBottomWallCollision();
+				}
+			}
+		}
+		
+		/* If the mouse button is pushed down, the round starts */
+		public void mousePressed(MouseEvent e) {
+			roundStarted = true;
+//			System.out.println("I just clicked");
+//			System.out.println("Click!");
+		}
+		
+		/* Returns a GLabel that says click anywhere to launch the ball.
+		 * Call this method between rounds while waiting for mouse click */
+		private GLabel addClickAnywhere() {
+			GLabel clickAnywhere = new GLabel("Click anywhere to launch the ball");
+			clickAnywhere.setFont("Arial-14");
+			clickAnywhere.setLocation((APPLICATION_WIDTH - clickAnywhere.getWidth()) / 2, 200); 
+			clickAnywhere.setColor(Color.BLACK);
+			return clickAnywhere;
+		}
+		
+		/* Prepares the mouse to control the paddle */
 		private void initializeMouse() {
 			addMouseListeners();
-			startX = getXForPaddle() + (PADDLE_WIDTH / 2);
-			startY = getYForPaddle() + (PADDLE_HEIGHT / 2);
-			paddle = getElementAt(startX, startY);
 		}
 		
 	/** Runs the Breakout program. */
 		public void run() {
-			this.resize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
+			this.resize(APPLICATION_WIDTH + (BRICK_WIDTH / 2), APPLICATION_HEIGHT);
 			setUpGame();
-//			playGame();
+			playGame();
 		}
 		
 		/* Instance variables */
-		private double lastX; /* The last mouse X position */
 		private GObject paddle = createPaddle();
-		private double startX; /* The starting x position for the mouse */
-		private double startY; /* The starting y position for the mouse */
+		private GObject ball;
+		private GLabel clickAnywhere = addClickAnywhere();
+		private double vx, vy; /* Velocity of the ball */
+		private int failCounter = 0; /* How many times they have miss the ball and let it hit the bottom */
+		private boolean roundStarted = false; /* Whether or not the ball is currently moving */
+		private int numberOfBricksRemaining = NBRICKS_PER_ROW * NBRICK_ROWS; /* How many bricks are remaining so we know when the game is over */
+		
+		
+		private RandomGenerator rgen = RandomGenerator.getInstance();
 	}
 
-//	 TODO: GET MOUSE MOVING THE PADDLE
-
-/*Notes from lectures:
- * To see if the ball hit a brick, call getElementAt(location of brick? or ball or something?
- * and if it doesn't return null it means there is a brick there. It will return a GRect object
- * which you will then remove.
- * 
- *  Use waitForClick() to pause until they click the mouse to get the game start again
- *  after they lose a life */
+/*TODO:
+ * MAKE IT SHOW HOW MANY LIVES YOU HAVE LEFT
+ * FIX BUG THAT HAPPENS WHEN YOU MOVE PADDLE SIDEWAYS INTO THE BALL
+ */
